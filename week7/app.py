0afa92acfbaa02ddceb90a09d066dfbd6e8d043c
cdr731 <chrisreutz@yahoo.com>
2026-05-12 19:32:06 -0700

    week7 Tuesday class

---
+++ week7/app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,url_for,flash,redirect
+from datetime import datetime
+import sqlite3 as sq
+import sqlite_helper as sqlh
+
+if __name__ == "__main__":
+   
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        
+        db_name=":memory:"
+        with sq.connect(database=db_name) as conn:
+
+            #Create a table
+            sql = '''CREATE TABLE IF NOT EXISTS user (userId INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT NOT NULL)'''
+            sqlh.sql_execute_query(CONN=conn,
+                                   SQL=sql)
+                 
+            #Query for a row
+            sql='''SELECT * FROM user ORDER BY 2 LIMIT 1'''
+            one_row = sqlh.sql_query(CONN=conn,
+                                   SQL=sql)
+            
+            #Check if there are already rows in the recordset - don't insert
+            if not one_row:
+                sql='''INSERT INTO user (user_name) VALUES ('Bob'),('Alice'),('Mickey')'''
+                sqlh.sql_execute_query(CONN=conn,
+                                    SQL=sql) 
+
+            #Call the users to pass to the webpage    
+            sql='''SELECT * FROM user ORDER BY 2'''
+            users=sqlh.sql_query(CONN=conn,
+                                   SQL=sql)
+                                 
+        title="LIST USERS"
+        now=datetime.now()
+        current_dt=now.strftime("%m/%d/%Y %H:%M")
+
+        return render_template("student_jinja_index.html",
+                                current_dt=current_dt,
+                                users=users)
+        
+    if __name__ == "__main__": 
+
+        app.run(host='0.0.0.0', port=5000, debug=True)
