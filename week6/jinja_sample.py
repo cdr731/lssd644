3cb3572a20cd8d82d659e0617b38d7c6e1990fed
cdr731 <chrisreutz@yahoo.com>
2026-05-13 20:55:00 -0700

    week6 homework completion

--- week6/jinja_sample.py
+++ week6/jinja_sample.py
@@ -7,13 +7,18 @@ if __name__ == "__main__":
     @app.route('/')
     def index():
 
-        messages=[{'user':"Bob",
-         'content':'Message One'},
-         {'user':'Alice','content':'Message Two'},
-         {'user':'Betty','content':'Betty\'s recipe of the day'}]
+        messages=[{'user':"Bob Newhart",
+         'content':'The late Bob Newhart starred in the 1970s \'The Bob Newhart Show\' and the 1980s \'Newhart\' sitcoms.'},
+         {'user':'Alice Hyatt',
+         'content':'Alice Hyatt was a waitress at Mel\'s Diner in the 1970s \'Alice\' sitcom. The character was played by the late Linda Lavin.'},
+         {'user':'Betty White',
+         'content':'Betty White was an active actress until she passed away before her 100th birthday. Her comedic talent was loved by generations of fans.'},
+         {'user':'Chris Reutz',
+         'content':'I know what you are thinking: \'He is not a celebrity.\' Yes, you are right. But if I could be a character, I would be R2-D2!'}
+        ]
         
-        page_name="Messages Page"
-        page_title="Messages Title"
+        page_name="Chris\' Celebrity Main Page"
+        page_title="Chris\' Celebrity Site"
 
         return render_template("test.html",
                                page_name=page_name,
